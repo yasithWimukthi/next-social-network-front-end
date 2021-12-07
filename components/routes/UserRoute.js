67aeb499dd0a6693ec5abfe5c898c28f5a85e703
yasith wimukthi <it19966922@my.sliit.lk>
2021-12-07 11:47:22 +0530

    implement user dashboard

---
+++ components/routes/UserRoute.js
@@ -0,0 +1,49 @@
+import {useContext, useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import axios from "axios";
+import {SyncOutlined} from "@ant-design/icons";
+import {UserContext} from "../../context";
+
+
+const UserRoute = ({children}) => {
+
+    const [state] = useContext(UserContext);
+    const [ok, setOk] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (state && state.token) getCurrentUser();
+    }, [state && state.token]);
+
+    const getCurrentUser = async () => {
+        try {
+            setLoading(true);
+            const {data} = await axios.get("http://localhost:8000/api/auth/current-user",{
+                headers: {
+                    'Authorization': `Bearer ${state.token}`
+                }
+            });
+            if (data.ok) setOk(true)
+            setLoading(false);
+        } catch (e) {
+            router.push('/login');
+        }
+    }
+
+    process.browser && state === null && setTimeout(()=>{
+        getCurrentUser()
+    },1000)
+
+    return (
+        !ok ? <SyncOutlined
+            spin
+            className="d-flex justify-content-center display-1 text-primary p-5"
+        /> : (
+            <>
+                {children}
+            </>
+        )
+    )
+}
+
+export default UserRoute;
---
+++ pages/user/dashboard.js
@@ -0,0 +1,12 @@
+import UserRoute from "../../components/routes/UserRoute";
+
+const Dashboard = () =>{
+
+    return(
+        <UserRoute>
+            <p>dashboard</p>
+        </UserRoute>
+    )
+}
+
+export default Dashboard;
